6dccdbd0a2404d8bf28dfbd23b87c81fa947e338
root <Part4@github.com>
2024-01-09 16:58:25 -0800

    adding \n in few lines

--- binary_trees.h
+++ binary_trees.h
@@ -13,7 +13,6 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
 typedef struct binary_tree_s
 {
 	int n;
@@ -23,8 +22,11 @@ typedef struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
 typedef struct binary_tree_s bst_t;
+
 typedef struct binary_tree_s avl_t;
+
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
